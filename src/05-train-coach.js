315aa2991c4835189fcf7943128671ebff195dc9
Neha Chopra <chopraneha211@gmail.com>
2026-02-17 05:49:17 +0530

    Learn find methods of array

--- src/05-train-coach.js
+++ src/05-train-coach.js
@@ -49,20 +49,32 @@
  */
 export function findPassenger(passengers, name) {
   // Your code here
+  if (!Array.isArray(passengers)|| passengers.length === 0 || typeof name !== "string" || name.trim() === "") return undefined
+  return passengers.find(passenger => passenger.name.toLowerCase() === name.toLowerCase())
 }
 
 export function getPassengerIndex(passengers, name) {
   // Your code here
+  if (!Array.isArray(passengers)|| passengers.length === 0 || typeof name !== "string" || name === "") return -1;
+
+  return passengers.findIndex(passenger => passenger.name.toLowerCase() === name.toLowerCase())
 }
 
 export function isAnyWaitlisted(passengers) {
   // Your code here
+  if (!Array.isArray(passengers)|| passengers.length === 0) return false
+
+  return passengers.some(passenger => passenger.status === "waitlisted")
 }
 
 export function areAllConfirmed(passengers) {
   // Your code here
+  if (!Array.isArray(passengers)|| passengers.length === 0) return false
+  return passengers.every(passenger => passenger.status === "confirmed")
 }
 
 export function getWaitlistedPassengers(passengers) {
   // Your code here
+  if (!Array.isArray(passengers)) return []
+  return passengers.filter(passenger => passenger.status === "waitlisted");
 }
